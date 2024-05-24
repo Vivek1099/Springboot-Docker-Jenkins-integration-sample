5edbbfdc320f134ccf605f1a383a1541416cd82e
Vivek1099 <viveksawaiyan8@gmail.com>
2024-05-24 21:28:01 +0530

    integration with jenkins and docker

---
+++ src/main/java/com/Springboot_Github_Jenkins_Docker/Springboot_Github_Jenkins_Docker/SpringbootGithubJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot_Github_Jenkins_Docker.Springboot_Github_Jenkins_Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Springboot_Github_Jenkins_Docker/Springboot_Github_Jenkins_Docker/SpringbootGithubJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot_Github_Jenkins_Docker.Springboot_Github_Jenkins_Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
