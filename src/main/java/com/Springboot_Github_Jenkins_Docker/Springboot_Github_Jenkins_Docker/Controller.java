e5ee11071e638e71b5230ac54cbf7b9e352235be
Vivek1099 <viveksawaiyan8@gmail.com>
2024-05-24 22:49:26 +0530

    adding controller

---
+++ src/main/java/com/Springboot_Github_Jenkins_Docker/Springboot_Github_Jenkins_Docker/Controller.java
@@ -0,0 +1,21 @@
+package com.Springboot_Github_Jenkins_Docker.Springboot_Github_Jenkins_Docker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller 
+{
+	@RequestMapping("/test")
+	public String test()
+	{
+		return "This is a demo test";
+	}
+	
+	@RequestMapping("/jenkins")
+	public String demo()
+	{
+		return "Sample integration test successful";
+	}
+
+}
